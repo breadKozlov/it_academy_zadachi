73776dd812abfe77dad423090749311037442c74
breadKozlov <killersarequiet@gmail.com>
2022-10-26 21:01:44 +0300

    Homework

--- src/exam/NaOzcenkuNumSix.java
+++ src/exam/NaOzcenkuNumSix.java
@@ -19,7 +19,7 @@ public class NaOzcenkuNumSix {
         System.out.println("The last word after replaceLast - " + rep.last());
         System.out.println();
 
-        //отсебятин
+        //отсебятина
         Pair<String, String> prob = Pair.createNewPairWithTwoType("Hello,", "world!");
         System.out.println(prob.first() + " " + prob.last());
     }
